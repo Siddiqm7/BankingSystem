2195f396f25ac6916a5913ded07750942200d9ee
Siddiqm7 <smohamedsiddiq057@gmail.com>
2026-05-19 17:16:34 +0530

    Add resource routes for accounts with renew route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::resource('accounts', AccountController::class);
+Route::post('accounts/{id}/renew', [AccountController::class, 'renew'])->name('accounts.renew');
