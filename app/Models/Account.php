082eceff8b7ef061bf8cd0939fa3373d1d4aeb7f
Siddiqm7 <smohamedsiddiq057@gmail.com>
2026-05-20 20:40:29 +0530

    Add active and inactive query scopes to Account model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,25 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 
 class Account extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-    'user_id',   
-    'name',
-    'age',
-    'gender',
-    'account_type',
-    'account_opening_date',
-    'account_status',
-    'account_number',
-    'current_account_balance',
-    'savings_account_balance',
-    'address',
-    'phone',
-    'email',
-];
+        'user_id',
+        'name',
+        'age',
+        'gender',
+        'account_type',
+        'account_opening_date',
+        'account_status',
+        'account_number',
+        'current_account_balance',
+        'savings_account_balance',
+        'address',
+        'phone',
+        'email',
+    ]; 
+    #[Scope]
+    protected function active(Builder $query): void
+    {
+        $query->where('account_status', 'active');
+    }
 
+    #[Scope]
+    protected function inactive(Builder $query): void
+    {
+        $query->where('account_status', 'inactive');
+    }
 }
